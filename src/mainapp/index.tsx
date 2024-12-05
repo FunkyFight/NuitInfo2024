4c3cf20f25a0a3bf04121168abd5bd7887481529
Spishewi <spishewi@gmail.com>
2024-12-06 00:34:10 +0100

    firby maggle

--- src/mainapp/index.tsx
+++ src/mainapp/index.tsx
@@ -5,11 +5,15 @@ import { useState } from "react";
 
 export default function MainApp() {
     const [body_part, set_body_part] = useState("heart");
+    const firby = "https://cdn.idealo.com/folder/Product/204404/0/204404007/s4_produktbild_gross/hasbro-furby-furblets-luv-lee.jpg";
     return <>
       <div className="w-full">
         <div className="w-full grid grid-cols-2">
           {
-             body_part == "heart" ? <Wheel img1="" img2="" img3="" img_bodypart=""/> : <></>
+             body_part == "heart" ? <Wheel img1={firby} img2={firby} img3={firby} img_bodypart={firby}/> : 
+             body_part == "default" ? <Wheel img1={firby} img2={firby} img3={firby} img_bodypart={firby}/> : 
+             
+             <></>
           }
           <div>
           <h1 className="text-3xl font-bold underline">
