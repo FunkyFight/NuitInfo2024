326f67c69b9c74c837ced2a7805835d62a7dee71
Spishewi <spishewi@gmail.com>
2024-12-05 20:08:27 +0100

    base mainapp

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,16 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router';
 import App from './App.tsx';
+import MainApp from './mainapp/index.tsx';
 import Captcha from './captcha/index.tsx';
-import './index.css';
+//import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path='/' Component={App} />
+        <Route path='/main' Component={MainApp} />
         <Route path='/captcha' Component={Captcha} />
       </Routes>
     </BrowserRouter>
