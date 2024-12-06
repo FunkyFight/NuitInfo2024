2194c6a098e8ee23d03d331a571c28ff4530437f
FunkyFight <alexisdeschampsgamer@gmail.com>
2024-12-06 06:27:56 +0100

    FIX FIX

--- src/assets/data.ts
+++ src/assets/data.ts
@@ -5,13 +5,9 @@ export const data = {
         img2: ["", "", ""],
         img3: ["", "", ""],
         img_bodypart: ["img/bodyparts/Coeur_1.webp", "img/bodyparts/Coeur_2.webp", "img/bodyparts/Coeur_3.webp"],
-        description: `Le cœur agit comme la pompe centrale du système circulatoire, maintenant un flux continu de sang qui transporte l’oxygène, les nutriments et les déchets à travers tout le corps.
-Courants océaniques similaires : Les courants océaniques, tels que le Gulf Stream, jouent un rôle similaire en transportant chaleur, énergie et nutriments à travers le globe, influençant les écosystèmes marins et terrestres.
-Analogies spécifiques :
-La pression sanguine, régulée par le cœur, peut être comparée à la dynamique des vagues et des marées.
-Les cycles réguliers des battements cardiaques rappellent les rythmes des marées, influencées par des forces constantes comme la gravité.
-Impact global : Si le cœur s'arrête, le corps entier en souffre. De même, une perturbation des courants océaniques, comme la circulation thermohaline, peut avoir des conséquences catastrophiques pour le climat mondial.
-`
+        description: [`Le cœur, comme les courants marins, est le moteur qui maintient le système en équilibre. Avant l’intervention humaine, les courants marins répartissaient la chaleur et les nutriments de manière harmonieuse, favorisant la biodiversité et la régulation climatique, tout comme le cœur distribue le sang et l’oxygène pour nourrir l’ensemble du corps.`, 
+        `La pollution plastique et le réchauffement climatique perturbent les courants marins. Leur ralentissement ou leur modification, semblable à une arythmie cardiaque, provoque des déséquilibres écologiques (blanchissement des coraux, migrations forcées). Un coeur en difficulté commence à battre de manière irrégulière, limitant la distribution d'oxygène, ce qui entraîne des symptômes dans tout le corps.`,
+        `Les courants marins pourraient s’effondrer complètement (comme un coeur en arrêt), entraînant un blocage des écosystèmes océaniques. Cela provoquerait une chaîne de réactions catastrophiques : extinction massive des espèces, acidification, et effondrement des cycles climatiques, à l’image du corps qui s’effondre sans circulation sanguine.`]
     },
     lungs:{
         title:"Poumons",
@@ -19,13 +15,9 @@ Impact global : Si le cœur s'arrête, le corps entier en souffre. De même, une
         img2: ["", "", ""],
         img3: ["", "", ""],
         img_bodypart: ["img/bodyparts/Poumon_1.webp", "img/bodyparts/Poumon_2.webp", "img/bodyparts/Poumon_3.webp"],
-        description:`Les poumons humains sont essentiels pour échanger l'oxygène et le dioxyde de carbone avec l'environnement, tout comme les océans produisent et absorbent ces gaz essentiels.
-
-Phytoplancton et photosynthèse : Environ 50 à 80 % de l'oxygène mondial provient du phytoplancton océanique. Ces micro-organismes, tout comme les alvéoles dans nos poumons, fonctionnent à une échelle microscopique mais ont un impact global.
-Structure en miroir : Les alvéoles pulmonaires, avec leur immense surface d’échange, rappellent la surface d’échange des océans, où les gaz et les nutriments se mélangent constamment entre l’eau et l’atmosphère.
-Lien avec la pollution : Tout comme la pollution des océans nuit à la biodiversité marine, les poumons souffrent des particules et des gaz toxiques présents dans l’air.
-Rôle vital commun : Les deux sont des régulateurs chimiques pour leur système : les poumons pour le corps et les océans pour la planète.
-`
+        description:[`Le cerveau et les écosystèmes marins jouent le rôle de centre de contrôle et de régulation. Les récifs coralliens, riches en biodiversité, agissent comme des hubs qui maintiennent l'équilibre de la vie marine, tout comme le cerveau orchestre toutes les fonctions du corps de manière précise et interconnectée.`,
+        `Les récifs coralliens blanchissent à cause de l'augmentation des températures et de la pollution. Ils deviennent fragiles et moins efficaces, à l’image d’un cerveau qui subit un stress chronique ou des lésions, perdant ses capacités de coordination et de régulation.`,
+        `La mort des récifs coralliens entraînerait l’effondrement de tout l’écosystème marin, comme un cerveau en dégénérescence avancée (maladie d’Alzheimer ou encéphalopathie). Les signaux ne circuleraient plus correctement, entraînant une désorganisation systémique.`]
     },
     system:{
         title:"Système lymphatique",
@@ -33,14 +25,10 @@ Rôle vital commun : Les deux sont des régulateurs chimiques pour leur système
         img2: ["", "", ""],
         img3: ["", "", ""],
         img_bodypart: ["img/bodyparts/Sys_lymph_1.webp", "img/bodyparts/Sys_lymph_2.webp", "img/bodyparts/Sys_lymph_3.webp"],
-        description:`Le système lymphatique agit comme un réseau de drainage et de filtration qui protège le corps contre les infections et élimine les déchets, rappelant les fonctions d’autorégulation des océans.
+        description:[`Le système lymphatique, tout comme les réseaux de transport subtils dans l’océan (courants secondaires, zones de transition comme les gyres océaniques), agit comme un système de drainage et de protection. Il redistribue les nutriments et élimine les déchets. Ces courants plus discrets jouent un rôle clé dans le transport des sédiments, des nutriments, et dans la purification naturelle des eaux, à l’image de la lymphe qui transporte les cellules immunitaires et élimine les toxines.`,
+        `La pollution par les déchets plastiques, les hydrocarbures, et les eaux usées surcharge les zones de transition et les gyres océaniques. Les courants secondaires commencent à accumuler des polluants, créant des vortex de déchets, comme le « Great Pacific Garbage Patch ». Cela reflète un système lymphatique surchargé, où l’excès de toxines bloque la circulation de la lymphe, affaiblissant l’immunité et favorisant les infections.`,
+        `Si la pollution océanique continue, les gyres océaniques et les courants secondaires pourraient devenir totalement dysfonctionnels. L'accumulation de déchets plastiques, les toxines chimiques et l'excès de nutriments (eutrophisation) empêcheraient les processus naturels de purification et d’équilibre. Cela pourrait provoquer la mort des zones océaniques entières (zones mortes hypoxiques), à l’image d’un système lymphatique effondré, où les toxines s’accumulent dans les tissus corporels, conduisant à des inflammations chroniques, des maladies auto-immunes, ou même la mort.`]
 
-Purification :
-Le système lymphatique élimine les toxines et recycle les fluides corporels. Les océans, quant à eux, filtrent les polluants grâce à des processus naturels tels que la sédimentation et la biodégradation par les organismes marins.
-Distribution des éléments essentiels :
-Les vaisseaux lymphatiques transportent les graisses et autres nutriments absorbés dans l’intestin, tout comme les courants marins redistribuent le plancton et les minéraux essentiels pour soutenir la chaîne alimentaire océanique.
-Résilience partagée : Bien qu’efficaces, les deux systèmes peuvent être submergés : l’océan par une pollution excessive et le système lymphatique par des infections chroniques.
-`
     },
     kidneys:{
         title:"Reins",
@@ -48,17 +36,9 @@ Résilience partagée : Bien qu’efficaces, les deux systèmes peuvent être su
         img2: ["", "", ""],
         img3: ["", "", ""],
         img_bodypart: ["img/bodyparts/Reins_1.webp", "img/bodyparts/Reins_2.webp", "img/bodyparts/Reins_3.webp"],
-        description:`Les reins maintiennent l'équilibre hydrique et électrolytique dans le corps, tout comme les océans régulent la salinité et les niveaux d'eau sur Terre.
-
-Homéostasie commune :
-Les reins filtrent le sang pour éliminer les déchets tout en conservant les éléments essentiels, comme le sodium et le potassium. Les océans, grâce aux courants et aux cycles d’évaporation et de précipitation, régulent les niveaux de sel et les nutriments dans l’eau.
-Fonction en boucle :
-Le cycle urinaire, où l'eau et les électrolytes sont recyclés ou excrétés, peut être comparé au cycle de l'eau dans les océans, où l'évaporation, les précipitations et les écoulements terrestres maintiennent l'équilibre global.
-Fragilité : Une défaillance rénale entraîne une accumulation toxique dans le corps, tout comme une acidification ou une pollution excessive des océans menace la vie marine.
-
-Conclusion générale
-Les organes humains et les océans partagent des rôles fondamentaux dans leurs systèmes respectifs : maintenir l'équilibre, fournir de l'énergie et éliminer les déchets. Ces comparaisons montrent à quel point les processus de la nature, qu’ils soient microscopiques ou macroscopiques, suivent des principes similaires d’interdépendance et de régulation. Protéger nos organes internes et préserver les océans sont deux facettes de la même responsabilité : la sauvegarde de la vie.
-`
+        description:[`Le foie et les zones de filtration comme les mangroves jouent un rôle essentiel dans la purification et l’équilibre. Avant l'intervention humaine, ces régions filtraient les polluants naturels, protégeaient les côtes, et maintenaient la qualité des eaux, tout comme le foie nettoie le sang et élimine les toxines.`,
+        `La destruction des mangroves et la pollution excessive saturent ces filtres naturels. C'est comparable à un foie qui doit traiter un excès d’alcool ou de toxines : il devient engorgé, moins performant, et laisse les substances nocives s’accumuler.`,
+        `La disparition des mangroves entraînerait une incapacité totale à filtrer les polluants, exacerbant l'érosion côtière et les inondations. Cela correspond à un foie défaillant (cirrhose ou insuffisance hépatique), incapable de protéger le corps, conduisant à des complications mortelles.`]
     },
     brain:{
         title:"Cerveau",
@@ -66,15 +46,8 @@ Les organes humains et les océans partagent des rôles fondamentaux dans leurs 
         img2: ["", "", ""],
         img3: ["", "", ""],
         img_bodypart: ["img/bodyparts/Cerveau_1.webp", "img/bodyparts/Cerveau_2.webp", "img/bodyparts/Cerveau_3.webp"],
-        description:`
-Le cerveau humain est un réseau incroyablement complexe, interconnecté, où chaque cellule contribue à la fonction globale, tout comme les océans sont constitués d'écosystèmes interdépendants.
-
-Complexité et communication :
-Les neurones dans le cerveau transmettent des signaux électriques via des synapses, un processus comparable à la manière dont les courants océaniques transportent l'énergie thermique et les nutriments.
-Le cerveau est également un centre de prise de décisions, comme les océans qui influencent le climat mondial, les cycles de vie et la biodiversité.
-Interaction des systèmes :
-Dans le cerveau, différents lobes remplissent des fonctions spécifiques tout en collaborant. De même, les écosystèmes marins (coraux, zones abyssales, estuaires) jouent chacun un rôle unique dans la survie globale de l’océan.
-Vulnérabilité commune : Les deux sont sensibles à des déséquilibres : un trauma crânien peut perturber le fonctionnement cérébral, tout comme les changements climatiques ou la surpêche affectent les océans.
-`
+        description: [`Le cerveau et les écosystèmes marins jouent le rôle de centre de contrôle et de régulation. Les récifs coralliens, riches en biodiversité, agissent comme des hubs qui maintiennent l'équilibre de la vie marine, tout comme le cerveau orchestre toutes les fonctions du corps de manière précise et interconnectée.`,
+        `Les récifs coralliens blanchissent à cause de l'augmentation des températures et de la pollution. Ils deviennent fragiles et moins efficaces, à l’image d’un cerveau qui subit un stress chronique ou des lésions, perdant ses capacités de coordination et de régulation.`,
+        `La mort des récifs coralliens entraînerait l’effondrement de tout l’écosystème marin, comme un cerveau en dégénérescence avancée (maladie d’Alzheimer ou encéphalopathie). Les signaux ne circuleraient plus correctement, entraînant une désorganisation systémique.`]
     }
 }
