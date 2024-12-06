8675a9dd984a461b32df3fa883ea2a78130f129e
Maximal20 <maxflip@outlook.fr>
2024-12-06 01:32:53 +0100

    Add wheel to drawer

--- src/mainapp/index.tsx
+++ src/mainapp/index.tsx
@@ -2,7 +2,17 @@ import Human from "@/components/ui/human";
 import Wheel from "@/components/ui/wheel";
 
 import { useState } from "react";
-
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer"
+import { Button } from "@/components/ui/button"
 export default function MainApp() {
     const [body_part, set_body_part] = useState("heart");
     const firby = "https://cdn.idealo.com/folder/Product/204404/0/204404007/s4_produktbild_gross/hasbro-furby-furblets-luv-lee.jpg";
@@ -22,10 +32,26 @@ export default function MainApp() {
           <Human />
           </div>
         </div>
-        
       </div>
       
       
       
+      <Drawer>
+        <DrawerTrigger>Open</DrawerTrigger>
+        <DrawerContent className="bg-cyan-50 h-[90%]">
+          <div >
+            <DrawerHeader className="justify-items-center">
+                <DrawerTitle >PARTIE DU CORPS</DrawerTitle>
+            </DrawerHeader>
+                <DrawerFooter>
+                  <DrawerDescription className="text-center">
+                    <p>
+                      Les yeux sont comme les poissons qui vivent dans l'océan. 
+                    </p>
+                  </DrawerDescription>
+                </DrawerFooter>
+          </div>
+        </DrawerContent>
+      </Drawer>
     </>;
   }
