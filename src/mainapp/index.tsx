48e1d25114e5f86550613c8f78e129baa8eda599
Maximal20 <maxflip@outlook.fr>
2024-12-06 04:04:53 +0100

    Added scrollarea to description

--- src/mainapp/index.tsx
+++ src/mainapp/index.tsx
@@ -14,12 +14,14 @@ import {
 } from "@/components/ui/drawer"
 import { Slider } from "@/components/ui/slider"
 import { DialogDescription, DialogTitle } from "@radix-ui/react-dialog";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 export default function MainApp() {
     const [body_part, set_body_part] = useState("heart");
     const firby = "https://cdn.idealo.com/folder/Product/204404/0/204404007/s4_produktbild_gross/hasbro-furby-furblets-luv-lee.jpg";
     return <>
       <div className="w-full h-dvh">
+      
         <div className="w-full grid grid-cols-2 h-dvh">
           {
              body_part == "heart" ? <Wheel img1={firby} img2={firby} img3={firby} img_bodypart={firby}/> : 
@@ -31,9 +33,9 @@ export default function MainApp() {
           <Human body_part_setter={set_body_part}/>
           </div>
         </div>
-        <Drawer>
+        <Drawer >
         <DrawerTrigger>Open</DrawerTrigger>
-        <DrawerContent className="bg-cyan-50 h-[99%] rounded-t-[500px] overflow-hidden p-0 border-0">
+        <DrawerContent className="bg-cyan-50 max-h-[99%] rounded-t-[500px] overflow-hidden p-0 border-0">
           <div className="justify-items-center p-0">
                 {
                 body_part == "heart" ? <Wheel img1={firby} img2={firby} img3={firby} img_bodypart={firby}/> : 
@@ -42,10 +44,12 @@ export default function MainApp() {
                 }
             
           </div>
-          <div>
-              <HeartTitleSlider></HeartTitleSlider>
-              <HeartDescription></HeartDescription>
+          <div className="h-[60%]">
           
+              <HeartTitleSlider></HeartTitleSlider>
+          <ScrollArea className="h-[80%] w-[100%]">
+              <HeartDescription></HeartDescription>
+          </ScrollArea>
           </div>
         </DrawerContent>
       </Drawer>
@@ -67,9 +71,11 @@ function HeartSlider() {
 }
 
 function HeartDescription() {
-  return (<DialogDescription className="w-[90%] justify-self-center z-[50] py-1 my-5 bg-gradient-to-tr">
+  return (
+  <DialogDescription className="w-[90%] justify-self-center z-[50] py-1 my-5 bg-gradient-to-tr">
     <p className="text-justify">
-      Les yeux sont comme les poissons qui vivent dans l'océan. Les yeux sont comme les poissons qui vivent dans l'océan. Les yeux sont comme les poissons qui vivent dans l'océan. Les yeux sont comme les poissons qui vivent dans l'océan. v Les yeux sont comme les poissons qui vivent dans l'océan.
+    Les yeux sont comme les poissons qui vivent dans l'océan. Les yeux sont comme les poissons qui vivent dans l'océan. Les yeux sont comme les poissons qui vivent dans l'océan. Les yeux sont comme les poissons qui vivent dans l'océan. v Les yeux sont comme les poissons qui vivent dans l'océan.Les yeux sont comme les poissons qui vivent dans l'océan. Les yeux sont comme les poissons qui vivent dans l'océan. Les yeux sont comme les poissons qui vivent dans l'océan. Les yeux sont comme les poissons qui vivent dans l'océan. v Les yeux sont comme les poissons qui vivent dans l'océan.Les yeux sont comme les poissons qui vivent dans l'océan. Les yeux sont comme les poissons qui vivent dans l'océan. Les yeux sont comme les poissons qui vivent dans l'océan. Les yeux sont comme les poissons qui vivent dans l'océan. v Les yeux sont comme les poissons qui vivent dans l'océan.Les yeux sont comme les poissons qui vivent dans l'océan. Les yeux sont comme les poissons qui vivent dans l'océan. Les yeux sont comme les poissons qui vivent dans l'océan. Les yeux sont comme les poissons qui vivent dans l'océan. v Les yeux sont comme les poissons qui vivent dans l'océan.
     </p>
-  </DialogDescription>)
+  </DialogDescription>
+  )
 }
